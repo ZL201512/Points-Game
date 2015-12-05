8d0d13fc0a8bb5da0ebe7faaefd24e406d39d1f7
melilll <2391077399@qq.com>
2015-12-05 15:30:44 +0800

    delete demo

---
+++ src/turntableProject/js/package.js
@@ -0,0 +1,12 @@
+//
+HTMLElement.prototype.getFinalStyle = function(css){
+    if(window.getComputedStyle){
+        return getComputedStyle(this,"")[css];//第二个参数是伪类
+    }else if(Object.currentStyle){//兼容IE
+        return this.currentStyle[css];
+    }
+}
+//
+function $(id){
+    return document.getElementById(id);
+}
